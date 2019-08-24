9987a927b1e4ee298eb539eac3da86f4b5802c4e
aenemark <aenemark@nvidia.com>
2019-08-23 17:00:30 -0700

    built

--- src/index.js
+++ src/index.js
@@ -4,51 +4,68 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 import * as dat from 'dat.gui';
 
 window.onload = function() {
 
     /* global attributes */
-    var types = ['lines', 'donut', 'squares']
-    var selectedType = types[0]
-    var speed = 10
-    var color = '#ff0086'
-    var at1 = 50;
-    var at2 = 50;
-    var at3 = 50;
-
     var container;
-    var camera, scene, renderer;
+    var camera, scene, renderer, controls
+    var composer, renderer;
+
     var groupAll;
 
+    var datProp = {
+        type: ['lines', 'donut', 'squares'],
+        selectedType: null,
+        speed: 10,
+        color: '#ff0086',
+        at1: 50,
+        at2: 50,
+        at3: 50,
+    }
+
+    var bloomParam = {
+        exposure: 1,
+        bloomStrength: 1.5,
+        bloomThreshold: 0,
+        bloomRadius: 0
+    };
 
     /* dat gui */
     var gui = new dat.GUI();
-    var datProp = new DatProps(selectedType, color, speed, at1, at2, at3)
 
-    gui.add(datProp, 'type', types).onChange(function(value) {
-        var lineObj3D = new BuildLines(at1, at2, color)
-        groupAll.add(lineObj3D)
+    gui.add(datProp, 'type', datProp.type).onChange(function(value) {
+        clearScene()
+        if (value === 'lines') {
+            var lineObj3D = BuildLines(datProp.at1, datProp.at2, datProp.at3, datProp.color)
+            lineObj3D.position.x = -(datProp.at1 * 10) / 2 // offset left by half
+            lineObj3D.position.z = -(datProp.at3 * 10) / 2
+            groupAll.add(lineObj3D)
+        }
     })
 
     gui.addColor(datProp, 'color').onChange(function(value) {
-        color = value;
+
     })
 
     gui.add(datProp, 'speed', 0, 100).onChange(function(value) {
-        speed = value;
+
     })
 
     gui.add(datProp, 'at1', 0, 100).onChange(function(value) {
-        at1 = value;
+
     })
 
     gui.add(datProp, 'at2', 0, 100).onChange(function(value) {
-        at2 = value;
+
     })
 
     gui.add(datProp, 'at3', 0, 100).onChange(function(value) {
-        at3 = value;
+
     })
 
 
@@ -61,12 +78,13 @@ window.onload = function() {
 
         container = document.getElementById('content');
         scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xf0f0f0);
+        scene.background = new THREE.Color(0x353535);
+        scene.fog = new THREE.Fog(0x353535, 1, 1000)
 
         groupAll = new THREE.Group();
         scene.add(groupAll)
 
-        camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+        camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 2000);
         camera.position.set(0, 150, 500);
         scene.add(camera);
 
@@ -76,79 +94,119 @@ window.onload = function() {
         renderer = new THREE.WebGLRenderer({ antialias: true });
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.toneMapping = THREE.ReinhardToneMapping; // bloom
         container.appendChild(renderer.domElement);
 
+        // controls
+        controls = new OrbitControls(camera, renderer.domElement);
+        controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+        controls.dampingFactor = 0.05;
+        controls.screenSpacePanning = false;
+        controls.minDistance = 50;
+        controls.maxDistance = 1000;
+
+        // unreal
+        var renderScene = new RenderPass(scene, camera);
+
+        var bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+        bloomPass.threshold = bloomParam.bloomThreshold;
+        bloomPass.strength = bloomParam.bloomStrength;
+        bloomPass.radius = bloomParam.bloomRadius;
+        
+        composer = new EffectComposer(renderer);
+        composer.addPass(renderScene);
+        composer.addPass(bloomPass);
+
+        // resize
         window.addEventListener('resize', onWindowResize, false);
+
+        // remove loading notice
+        var loading = document.getElementById('loading');
+        loading.parentNode.removeChild(loading)
+
+        // helper
+        var gridHelper = new THREE.GridHelper(500, 50, 0x0000ff, 0x808080);
+        scene.add(gridHelper);
 
     }
 
     function clearScene() {
-
-        groupAll.remove(groupAll.children);
+        console.log("clearing...", groupAll.children)
+        groupAll.remove(...groupAll.children);
 
     }
 
 
     function animate() {
-        //requestAnimationFrame(animate);
-        //render();
+        requestAnimationFrame(animate);
+        controls.update();
+        composer.render();
     }
-
+// NOTE trying to get bloom filter working ...
+/*
     function render() {
         // NOTE: mesh should reference attributes here
         renderer.render(scene, camera);
     }
-
+*/
     function onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
+
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        camera.aspect = width / height;
         camera.updateProjectionMatrix();
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setSize(width, height);
+        composer.setSize(width, height);
     }
 
 };
 
-/* dat GUI props */
-const DatProps = function(type, color, speed, at1, at2, at3) {
-    this.type = type;
-    this.color = color;
-    this.speed = speed;
-    this.at1 = at1;
-    this.at2 = at2;
-    this.at3 = at3;
-}
-
 
 /* Line Shape Mesh Builder */
-const BuildLines = function(param1, param2, color) {
+function BuildLines(at1, at2, at3, color) {
 
-        console.log("buildLines:", param1, param2, color)
+    var lineGroup = new THREE.Group();
 
-        var segments = 1000;
+    // line group
+    for (var j = 0; j < at3; j++) {
+        // line
+        var segments = at1 * 10;
         var points = [];
+        points.push(new THREE.Vector2(0, 0))
 
-        for ( var i = 0; i < segments; i ++ ) {
+        for (var i = 0; i < segments; i++) {
+
             var x = i;
-            var y = Math.random() * 100        
-            var z = Math.random() * 10
-
-            // points
-            points.push( x, y, z );
+            var y = (Math.random() * at2) * Math.abs(Math.sin(i / 100)) + 10
+            points.push(new THREE.Vector2(x, y))
 
         }
+        points.push(new THREE.Vector2(segments - 1, 0))
 
-        // NOTE - need to specify as SHAPE?
+        // create shape from points 
+        var lineShape = new THREE.Shape(points)
+        lineShape.autoClose = true;
 
-        /*
-        console.log(points)
-        var material = new THREE.LineBasicMaterial( { color: color } );
-        var geometryPoints = new THREE.BufferGeometry().setFromPoints( points );
-        //geometryPoints.computeBoundingSphere();
+        // create outline
+        var pointsShape = lineShape.getPoints();
+        var geometryPoints = new THREE.BufferGeometry().setFromPoints(pointsShape);
+        var line = new THREE.Line(geometryPoints, new THREE.LineBasicMaterial({ color: color }));
+        line.position.z = j * 10;
+        lineGroup.add(line)
 
-        //this.line = new THREE.Line( geometryPoints, material );
-        */
+        // create fill
+        var geometry = new THREE.ShapeBufferGeometry(lineShape);
+        var material = new THREE.MeshPhongMaterial({ color: color, opacity: 0.2, transparent: true })
+        var mesh = new THREE.Mesh(geometry, material);
+        mesh.position.z = j * 10;
+        lineGroup.add(mesh)
+
+    }
+
+    return (lineGroup)
 }
 
 
 
 
-// 
+//
